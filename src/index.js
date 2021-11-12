0fd36b4a4f95d80ec5cc9751ffde041d8c80b322
Naveen V <velnaveen99@gmail.com>
2021-11-12 14:49:55 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import Config from 'config.js';
+import Output from 'output.js';
 import 'utils.js';
 import 'query.js';
 
@@ -15,7 +15,13 @@ app.use(express.json());
 app.use(cors());
 app.use(morgan('combined'));
 
-app.get('/contractEvent', (req, res) =>{
-    var provider = req.body.provider;
-    Config.init(provider);
+app.get('/contractEvent', (req, res) => {
+    var result = getContractEvent(req);
+    if(result) {
+        res.send(Output.jsonResult);
+    }
 });
+
+app.listen(3001, () => {
+    console.log('listening on port 3001');
+})
