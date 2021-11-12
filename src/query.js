ff2bee170346002c1286a7d363830a03e02e4c3c
Naveen V <velnaveen99@gmail.com>
2021-11-12 16:35:57 +0530

    Update query.js

--- src/query.js
+++ src/query.js
@@ -1,18 +1,18 @@
-import 'utils.js';
-import Output from 'output.js';
+const utils = require('./utils.js');
+const output = require('./output.js');
 
 async function getContractEvent(req){
     var contractAddress = req.body.contractAddress;
     var contractJson = req.body.contractJson;
-    var contract = getContract(
+    var contract = utils.getContract(
         contractJson,
         contractAddress
     );
     var fromBlock = req.body.fromBlock == null ? 'earliest' : req.body.fromBlock;
     var toBlock = req.body.toBlock == null ? 'latest' : req.body.toBlock;
-    var params = getParams(req.body.event, contractJson);
-    Output.reset();
-    Output.initParams(params);
+    var params = utils.getParams(req.body.event, contractJson);
+    output.Output.reset();
+    output.Output.initFields(params);
     var result = await contract.getPastEvents(
         req.body.event,
         {
@@ -22,8 +22,12 @@ async function getContractEvent(req){
     );
     result.forEach(res => {
         var rawData = res.raw.data;
-        var decodedRawData = rawDataDecode(params, rawData);
-        Output.jsonResult.push(decodedRawData);
+        var decodedRawData = utils.rawDataDecode(params, rawData);
+        utils.addToOutput(params, decodedRawData);
     });
     return true;
+}
+
+module.exports = {
+    'getContractEvent': getContractEvent
 }
